fc4b077c4f53d28acb6b875fc4bfc0d6fbce1aa3
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-06 18:33:22 -0300

    Agrega mapa

--- Codigo Fuente/js/PuntosVenta.js
+++ Codigo Fuente/js/PuntosVenta.js
@@ -1,8 +1,34 @@
 var PuntosVenta = {
-    init: () => {
-      $('#content').load('../pages/PuntosVenta.html');
-    },
-  };
-  
-  export default PuntosVenta;
-  
+  init: () => {
+    $('#content').load('../pages/PuntosVenta.html', () => {
+      $(bootstrap);
+    });
+  },
+};
+
+var bootstrap = function () {
+  //Para el ejemplo
+  var url = Config.url;
+  var urlLocations = '/locations/';
+  var urlStates = '/weatherstates/';
+  var location_id = '4852';
+
+  var map = createMap('mapPuntosVenta');
+  var drawer = new Drawer();
+
+  fetch(url + urlLocations + location_id) //pedimos la ubicacion al servidor
+    .then((data) => data.json()) //parseamos la respuesta a un JSON
+    .then((data) => data['location']) // extraemos la ubicacion de la respuesta del servidor
+    .then((localizacion) => {
+      fetch(url + urlStates + localizacion.state_id) // resolvemos el estado
+        .then((data) => data.json())
+        .then((data) => {
+          // dibujamos la ubicacion con su estado
+          localizacion.state = data.state;
+          console.log(localizacion);
+          drawer.drawLocationInMap(localizacion, map);
+        });
+    });
+};
+
+export default PuntosVenta;
