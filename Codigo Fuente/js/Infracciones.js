fc76c9f787b42126bd46cd6e434ec6300962da2e
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-06 18:40:49 -0300

    Integra infracciones

--- Codigo Fuente/js/Infracciones.js
+++ Codigo Fuente/js/Infracciones.js
@@ -1,6 +1,15 @@
 var Infracciones = {
+  get: () => {
+    $.ajax({
+      url: 'https://infraccionesweb.herokuapp.com/api/',
+      success: function(response){
+        console.log(response);
+      }
+    });
+  },
   init: () => {
     $('#content').load('../pages/Infracciones.html');
+    Infracciones.get();
   },
 };
 
