25c402da05498770392bd0dc288546d2dec813f8
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-08 12:28:53 -0300

    Fix de mapa

--- Codigo Fuente/index.js
+++ Codigo Fuente/index.js
@@ -1,7 +1,7 @@
+import Infracciones from './js/Infracciones.js';
 import Menu from './js/Menu.js';
-import Saldo from './js/Saldo.js';
 
 $(document).ready(function () {
   Menu.init();
-  Saldo.init();
+  Infracciones.init();
 });
--- Codigo Fuente/js/Menu.js
+++ Codigo Fuente/js/Menu.js
@@ -1,5 +1,3 @@
-import Perfil from '../js/Perfil.js';
-import Saldo from '../js/Saldo.js';
 import Infracciones from '../js/Infracciones.js';
 import PuntosVenta from '../js/PuntosVenta.js';
 import EstacionamientoLibre from '../js/EstacionamientoLibre.js';
@@ -7,12 +5,6 @@ import EstacionamientoLibre from '../js/EstacionamientoLibre.js';
 var Menu = {
   onClickItem: (event) => {
     switch (event.target.text) {
-      case 'Perfil':
-        Perfil.init();
-        break;
-      case 'Saldo':
-        Saldo.init();
-        break;
       case 'Infracciones':
         Infracciones.init();
         break;
