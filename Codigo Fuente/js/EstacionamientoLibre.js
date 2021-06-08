437091dd6008ca9915cb7100be623ead08e894d7
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-08 12:45:02 -0300

    Configuracion de mapa en estacionamientos

--- Codigo Fuente/js/EstacionamientoLibre.js
+++ Codigo Fuente/js/EstacionamientoLibre.js
@@ -1,8 +1,34 @@
 var EstacionamientoLibre = {
-    init: () => {
-      $('#content').load('../pages/EstacionamientoLibre.html');
-    },
-  };
-  
-  export default EstacionamientoLibre;
-  
+  init: () => {
+    $('#content').load('../pages/EstacionamientoLibre.html', () => {
+      $(bootstrap);
+    });
+  },
+};
+
+var bootstrap = function () {
+  //Para el ejemplo
+  var url = Config.url;
+  var urlLocations = '/locations/';
+  var urlStates = '/weatherstates/';
+  var location_id = '4852';
+
+  var map = createMap('mapEstacionamientos');
+  var drawer = new Drawer();
+
+  fetch(url + urlLocations + location_id) //pedimos la ubicacion al servidor
+    .then((data) => data.json()) //parseamos la respuesta a un JSON
+    .then((data) => data['location']) // extraemos la ubicacion de la respuesta del servidor
+    .then((localizacion) => {
+      fetch(url + urlStates + localizacion.state_id) // resolvemos el estado
+        .then((data) => data.json())
+        .then((data) => {
+          // dibujamos la ubicacion con su estado
+          localizacion.state = data.state;
+          console.log(localizacion);
+          drawer.drawLocationInMap(localizacion, map);
+        });
+    });
+};
+
+export default EstacionamientoLibre;
