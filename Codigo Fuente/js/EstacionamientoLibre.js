8491a087254de90102a425736555207716e7ad59
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-15 23:03:21 -0300

    Agrega data para estacionamientos y zonas y se integra en el mapa

--- Codigo Fuente/js/EstacionamientoLibre.js
+++ Codigo Fuente/js/EstacionamientoLibre.js
@@ -1,113 +1,27 @@
+import Estacionamientos from '../mockData/Estacionamientos.js';
+import Zonas from '../mockData/Zonas.js';
+
 var EstacionamientoLibre = {
   init: () => {
     $('#content').load('../pages/EstacionamientoLibre.html', () => {
       $(mapa_est);
+      console.log(Estacionamientos);
+      console.log(Zonas);
     });
   },
 };
 
 var mapa_est = function () {
-
   var map = createMap('mapEstacionamientos');
   var drawer = new Drawer();
-  
-  drawer.drawLocationInMap(-34.5408936, -58.7131234, map);
-  drawer.drawLocationInMap(-34.5415345, -58.7139824, map);
-  drawer.drawLocationInMap(-34.5389316, -58.7154211, map);
-  drawer.drawLocationInMap(-34.5398986, -58.7145535, map);
-  drawer.drawLocationInMap(-34.5395412, -58.7124483, map);
 
-  drawer.drawLocationInMap(-34.533710789745264, -58.70683233493153, map);
-  drawer.drawLocationInMap(-34.532893251822564, -58.70589406359898, map);
-  drawer.drawLocationInMap(-34.53240272521586, -58.70894344542978, map);
-  drawer.drawLocationInMap(-34.531258151895535, -58.708781052314535, map);
+  Estacionamientos.map((estacionamiento) => {
+    return drawer.drawLocationInMap(estacionamiento.lat, estacionamiento.lon, map);
+  });
 
-  drawer.drawLocationInMap(-34.51642595414828, -58.709890536901966, map);
-  drawer.drawLocationInMap(-34.51544470406117, -58.71487059243629, map);
-  drawer.drawLocationInMap(-34.520677904313416, -58.71469015564158, map);
-  drawer.drawLocationInMap(-34.516842238572615, -58.708483129903136, map);
-  drawer.drawLocationInMap(-34.513214547248246, -58.709710100107245, map);
-  drawer.drawLocationInMap(-34.52133203123077, -58.71364362223218, map);
-
-  drawer.drawLocationInMap(-34.54294681566615, -58.707657511308284, map);
-  drawer.drawLocationInMap(-34.54103792464161, -58.70802229173243, map);
-  drawer.drawLocationInMap(-34.54091419871267, -58.70617693193965, map);
-  drawer.drawLocationInMap(-34.54179795131323, -58.70585506685952, map);
-
-  drawer.drawLocationInMap(-34.535706672629985, -58.71751681247522, map);
-  drawer.drawLocationInMap(-34.53498145448949, -58.71426331640657, map);
-  drawer.drawLocationInMap(-34.53592738994059, -58.71496505085276, map);
-  drawer.drawLocationInMap(-34.53632678168189, -58.71838441142687, map);
-  drawer.drawLocationInMap(-34.537377803424576, -58.71704473657507, map);
-
-  drawer.drawLocationInMap(-34.54235338877845, -58.7238713199616, map);
-  drawer.drawLocationInMap(-34.54143905526587, -58.72205956920964, map);
-  drawer.drawLocationInMap(-34.54382470897727, -58.72328441478844, map);
-  drawer.drawLocationInMap(-34.54285784434805, -58.721587493309485, map);
-  
-
-  L.polygon([
-    L.latLng(-34.515594, -58.705654),
-    L.latLng(-34.523503, -58.714062),
-    L.latLng(-34.519177, -58.719890),
-    L.latLng(-34.511089, -58.711374),
-    L.latLng(-34.514062, -58.707909),
-    L.latLng(-34.513824, -58.707584)
-  ],{
-    color: 'blue'
-  }).addTo(map);
-  
-  L.polygon([
-    L.latLng(-34.537242, -58.715502),
-    L.latLng(-34.540830, -58.710599),
-    L.latLng(-34.542881, -58.712874),
-    L.latLng(-34.539372, -58.717745)
-
-  ],{
-    color: 'green'
-  }).addTo(map);
-
-  L.polygon([
-    L.latLng(-34.535415775764804, -58.71359892033557),
-    L.latLng(-34.532949893513816, -58.71698923251725),
-    L.latLng(-34.5360963110612, -58.72030444284573),
-    L.latLng(-34.53852674881247, -58.716924859498235)
-
-  ],{
-    color: 'blue'
-  }).addTo(map);
-
-
-L.polygon([
-  L.latLng(-34.53038047960603, -58.70835784076817),
-  L.latLng(-34.53365077348545, -58.703916098429595),
-  L.latLng(-34.535029543736385, -58.70546105081425),
-  L.latLng(-34.53177702346993, -58.70990278892015)
-
-],{
-  color: 'green'
-}).addTo(map);
-
-L.polygon([
-  L.latLng(-34.54387606334513, -58.70802528983737),
-  L.latLng(-34.542254632189746, -58.71024165031463),
-  L.latLng(-34.53974476405871, -58.70760235665752),
-  L.latLng(-34.541441584603405, -58.70539221644057)
-
-],{
-  color: 'green'
-}).addTo(map);
-
-L.polygon([
-  L.latLng(-34.541666376441775, -58.72631839671998),
-  L.latLng(-34.539227192619286, -58.72370056073487),
-  L.latLng(-34.541754751325456, -58.72026733321341),
-  L.latLng(-34.54412316325387, -58.722906626870525)
-
-],{
-  color: 'blue'
-}).addTo(map);
-
+  Zonas.map((zona) => {
+    return L.polygon(zona.latLong, { color: zona.color }).addTo(map);
+  });
 };
 
 export default EstacionamientoLibre;
--- Codigo Fuente/mockData/Estacionamientos.js
+++ Codigo Fuente/mockData/Estacionamientos.js
@@ -1,33 +1,177 @@
 var Estacionamientos = [
+  /*Zona 1*/
   {
     id: 1,
     nombre: 'Estacionamiento 1',
-    lat: -34.5460985,
-    lon: -58.7104065,
+    lat: -34.5408936,
+    lon: -58.7131234,
   },
   {
     id: 2,
     nombre: 'Estacionamiento 2',
-    lat: -34.5460985,
-    lon: -58.7104065,
+    lat: -34.5415345,
+    lon: -58.7139824,
   },
   {
     id: 3,
     nombre: 'Estacionamiento 3',
-    lat: -34.5460985,
-    lon: -58.7104065,
+    lat: -34.5389316,
+    lon: -58.7154211,
   },
   {
     id: 4,
     nombre: 'Estacionamiento 4',
-    lat: -34.5460985,
-    lon: -58.7104065,
+    lat: -34.5398986,
+    lon: -58.7145535,
   },
   {
     id: 5,
     nombre: 'Estacionamiento 5',
-    lat: -34.5460985,
-    lon: -58.7104065,
+    lat: -34.5395412,
+    lon: -58.7124483,
+  },
+  /*Zona 2*/
+  {
+    id: 6,
+    nombre: 'Estacionamiento 6',
+    lat: -34.533710789745264,
+    lon: -58.70683233493153,
+  },
+  {
+    id: 7,
+    nombre: 'Estacionamiento 7',
+    lat: -34.532893251822564,
+    lon: -58.70589406359898,
+  },
+  {
+    id: 8,
+    nombre: 'Estacionamiento 8',
+    lat: -34.53240272521586,
+    lon: -58.70894344542978,
+  },
+  {
+    id: 9,
+    nombre: 'Estacionamiento 9',
+    lat: -34.531258151895535,
+    lon: -58.708781052314535,
+  },
+  /*Zona 3*/
+  {
+    id: 10,
+    nombre: 'Estacionamiento 10',
+    lat: -34.51642595414828,
+    lon: -58.709890536901966,
+  },
+  {
+    id: 11,
+    nombre: 'Estacionamiento 11',
+    lat: -34.51544470406117,
+    lon: -58.71487059243629,
+  },
+  {
+    id: 12,
+    nombre: 'Estacionamiento 12',
+    lat: -34.520677904313416,
+    lon: -58.71469015564158,
+  },
+  {
+    id: 13,
+    nombre: 'Estacionamiento 13',
+    lat: -34.516842238572615,
+    lon: -58.708483129903136,
+  },
+  {
+    id: 14,
+    nombre: 'Estacionamiento 14',
+    lat: -34.513214547248246,
+    lon: -58.709710100107245,
+  },
+  {
+    id: 15,
+    nombre: 'Estacionamiento 15',
+    lat: -34.52133203123077,
+    lon: -58.71364362223218,
+  },
+  /*Zona 4*/
+  {
+    id: 16,
+    nombre: 'Estacionamiento 16',
+    lat: -34.54294681566615,
+    lon: -58.707657511308284,
+  },
+  {
+    id: 17,
+    nombre: 'Estacionamiento 17',
+    lat: -34.54103792464161,
+    lon: -58.70802229173243,
+  },
+  {
+    id: 18,
+    nombre: 'Estacionamiento 18',
+    lat: -34.54091419871267,
+    lon: -58.70617693193965,
+  },
+  {
+    id: 19,
+    nombre: 'Estacionamiento 19',
+    lat: -34.54179795131323,
+    lon: -58.70585506685952,
+  },
+  /*Zona 5*/
+  {
+    id: 20,
+    nombre: 'Estacionamiento 20',
+    lat: -34.535706672629985,
+    lon: -58.71751681247522,
+  },
+  {
+    id: 21,
+    nombre: 'Estacionamiento 21',
+    lat: -34.53498145448949,
+    lon: -58.71426331640657,
+  },
+  {
+    id: 22,
+    nombre: 'Estacionamiento 22',
+    lat: -34.53592738994059,
+    lon: -58.71496505085276,
+  },
+  {
+    id: 23,
+    nombre: 'Estacionamiento 23',
+    lat: -34.53632678168189,
+    lon: -58.71838441142687,
+  },
+  {
+    id: 24,
+    nombre: 'Estacionamiento 24',
+    lat: -34.537377803424576,
+    lon: -58.71704473657507,
+  },
+  /*Zona 6*/
+  {
+    id: 25,
+    nombre: 'Estacionamiento 25',
+    lat: -34.54235338877845,
+    lon: -58.7238713199616,
+  },
+  {
+    id: 26,
+    nombre: 'Estacionamiento 26',
+    lat: -34.54143905526587,
+    lon: -58.72205956920964,
+  },
+  {
+    id: 27,
+    nombre: 'Estacionamiento 27',
+    lat: -34.54382470897727,
+    lon: -58.72328441478844,
+  },
+  {
+    id: 28,
+    nombre: 'Estacionamiento 28',
+    lat: -34.54285784434805,
+    lon: -58.721587493309485,
   },
 ];
 
