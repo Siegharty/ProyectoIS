0bc431a82560c98574ebad7196a8a1b093fd7b3f
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-16 23:12:09 -0300

    Finish him

--- Codigo Fuente/js/EstacionamientoActivo.js
+++ Codigo Fuente/js/EstacionamientoActivo.js
@@ -1,9 +1,26 @@
 var EstacionamientoActivo = {
-    init : () => {
-        $('#content').load('../pages/EstacionamientoActivo.html', () => {
+  init: () => {
+    $('#content').load('../pages/EstacionamientoActivo.html', () => {
+      $(mapa_est);
+    });
+  },
+};
 
-        });
-    }
+var mapa_est = function () {
+  var map = createMap('mapEstacionamientosActivos');
+  var drawer = new Drawer();
+
+  var estacionamiento = {
+    id: 12,
+    nombre: 'Estacionamiento 12',
+    estado: 1,
+    lat: -34.520677904313416,
+    lon: -58.71469015564158,
+  };
+
+  drawer.drawLocationInMap(estacionamiento.id, estacionamiento.nombre, estacionamiento.lat, estacionamiento.lon, map);
+
+  mapa.setView(marcadores[12]._latlng, 16);
 };
 
 export default EstacionamientoActivo;
--- Codigo Fuente/js/PuntosVenta.js
+++ Codigo Fuente/js/PuntosVenta.js
@@ -32,7 +32,7 @@ var PuntosVenta = {
     });
 
     const greenIcon = new L.Icon({
-      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
