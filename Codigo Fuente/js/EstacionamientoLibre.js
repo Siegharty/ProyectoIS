972190058b891422a2ce50f9638a3673791bf957
Kevin Nicolás Cabrera <kcabrera@lnexternos.com.ar>
2021-06-16 21:54:12 -0300

    Agrega estados a estacionamientos

--- Codigo Fuente/js/EstacionamientoLibre.js
+++ Codigo Fuente/js/EstacionamientoLibre.js
@@ -12,13 +12,41 @@ var EstacionamientoLibre = {
   },
   hacerZoomOnClick: () => {
     $('.list-group-item').click((event) => {
-      $(".list-group-item.active").removeClass('active');
-      $(event.target).addClass("active")
+      $('.list-group-item.active').removeClass('active');
+      $(event.target).addClass('active');
 
       var marcador = marcadores[event.target.getAttribute('value')];
       marcador.openPopup();
       mapa.setView(marcador._latlng, 16);
+    });
+  },
+  actualizarEstado: () => {
+    const redIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
 
+    const greenIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
+
+    Estacionamientos.map((estacionamiento, index) => {
+      const marker = marcadores[index + 1];
+
+      if (estacionamiento.estado == 0) {
+        return marker.setIcon(greenIcon);
+      } else {
+        return marker.setIcon(redIcon);
+      }
     });
   },
   init: () => {
@@ -41,6 +69,8 @@ var mapa_est = function () {
   Zonas.map((zona) => {
     return L.polygon(zona.latLong, { color: zona.color }).addTo(map);
   });
+
+  EstacionamientoLibre.actualizarEstado();
 };
 
 export default EstacionamientoLibre;
